eeb434e32d5c91d81a68e6bc2605fe709d40547d
Steven Yang <yangchenyun@gmail.com>
2012-12-18 13:06:42 +0800

    migrate the foxit errors from octokit errors

--- lib/faraday/response/raise_foxit_error.rb
+++ lib/faraday/response/raise_foxit_error.rb
@@ -3,18 +3,18 @@ require 'multi_json'
 
 # @api private
 module Faraday
-  class Response::RaiseOctokitError < Response::Middleware
+  class Response::RaiseFoxitError < Response::Middleware
     ERROR_MAP = {
-      400 => Octokit::BadRequest,
-      401 => Octokit::Unauthorized,
-      403 => Octokit::Forbidden,
-      404 => Octokit::NotFound,
-      406 => Octokit::NotAcceptable,
-      422 => Octokit::UnprocessableEntity,
-      500 => Octokit::InternalServerError,
-      501 => Octokit::NotImplemented,
-      502 => Octokit::BadGateway,
-      503 => Octokit::ServiceUnavailable
+      400 => Foxit::BadRequest,
+      401 => Foxit::Unauthorized,
+      403 => Foxit::Forbidden,
+      404 => Foxit::NotFound,
+      406 => Foxit::NotAcceptable,
+      422 => Foxit::UnprocessableEntity,
+      500 => Foxit::InternalServerError,
+      501 => Foxit::NotImplemented,
+      502 => Foxit::BadGateway,
+      503 => Foxit::ServiceUnavailable
     }
 
     def on_complete(response)
