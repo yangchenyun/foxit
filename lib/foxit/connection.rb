d52eee0aab628328f20c22e834ee3461962ad8e4
Steven Yang <yangchenyun@gmail.com>
2012-12-18 15:54:03 +0800

    added the connection config file

---
+++ lib/foxit/connection.rb
@@ -0,0 +1,38 @@
+require 'faraday_middleware'
+require 'faraday/response/raise_foxit_error'
+
+module Foxit
+  # @private
+  module Connection
+    private
+
+    def connection(options={})
+      options = {
+        :force_urlencoded => false,
+        :raw              => false,
+        :ssl              => { :verify => false },
+        :url              => api_endpoint
+      }.merge(options)
+
+      # TODO: Don't build on every request
+      connection = Faraday.new(options) do |builder|
+
+        builder.request :json
+
+        builder.use Faraday::Response::RaiseFoxitError
+        builder.use FaradayMiddleware::FollowRedirects
+        builder.use FaradayMiddleware::Mashify
+
+        builder.use FaradayMiddleware::ParseJson, :content_type => /\bjson$/
+
+        faraday_config_block.call(builder) if faraday_config_block
+
+        builder.adapter *adapter
+      end
+
+      connection.headers[:user_agent] = user_agent
+
+      connection
+    end
+  end
+end
